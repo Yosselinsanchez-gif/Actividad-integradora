04a8c26d332535c60a4696ed3146da03c5a1b523
Yosselinsanchez-gif <yossmor28@gmail.com>
2026-02-13 21:55:31 -0600

    app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Hola Mundo 🚀
-      </h1>
-    </div>
-  );
-}
